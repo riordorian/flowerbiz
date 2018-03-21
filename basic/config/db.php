aafdb63b6bd9aa5fb9065236abbbc7d94d4ac8a6
ftp <riordorian_fb@rembo.beget.ru>
2018-03-21 17:53:51 +0300

    uploads

--- basic/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-	'class' => 'yii\db\Connection',
-	'dsn' => 'mysql:host=127.0.0.1;port=8889;dbname=db_flower;',
-	'username' => 'root',
-	'password' => 'root',
-	'charset' => 'utf8',
-];
