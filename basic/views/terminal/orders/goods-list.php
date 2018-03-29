78a3db05dbfe2acee6bc3988b37f4c62bb6b5d40
riordorian <riordorian@mail.ru>
2018-03-29 20:39:41 +0300

    goods count bug fixing. Css bug fixing

--- basic/views/terminal/orders/goods-list.php
+++ basic/views/terminal/orders/goods-list.php
@@ -1,5 +1,10 @@
 <!--TODO: Сделать выравнивание блоков по высоте, если отличаются названия-->
-<?
+<? if( count($arGoods) > 2 ){
+	?><div class="text-center m-b-lg clearfix">
+		<a href="javascript:;" class="js-all-goods link btn btn-md btn-primary">Все товары</a>
+	</div><?
+}
+
 foreach($arGoods as $arGood){
 	echo $this->render('_good', [
 		'arGood' => $arGood
