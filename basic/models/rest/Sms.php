3a917eb472922af319d5e2b626f64a51b72a8a2f
riordorian <riordorian@mail.ru>
2018-08-30 15:23:42 +0300

    Sms sender 2

--- basic/models/rest/Sms.php
+++ basic/models/rest/Sms.php
@@ -35,7 +35,7 @@ class Sms
 				$obData->multiple = [];
 				foreach($arEvents as $arEvent){
 					$obSms = new SMSRU($arSmsSettings['API_ID']);
-					$obData->multi[$arEvent['PHONE']] = 'Добрый день, ' . $arEvent['CLIENT_NAME'] . '! :) Напоминаем о предстоящих событиях: ' . $arEvent['RECIPIENT'] . ' - ' . $arEvent['EVENT'] . '. Дарим Вам 200 бонусов на покупку букета до' . date('d.m.Y');
+					$obData->multi[$arEvent['PHONE']] = 'Добрый день, ' . $arEvent['CLIENT_NAME'] . '! :) Напоминаем о предстоящих событиях: ' . $arEvent['RECIPIENT'] . ' - ' . $arEvent['EVENT'] . '. Дарим Вам 200 бонусов на покупку букета до ' . date('d.m.Y');
 				}
 
 				$obSms->send_one($obData);
