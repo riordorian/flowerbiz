fa6dc9019918b41cf9a9968a07c6ec9665bd66fa
riordorian <riordorian@mail.ru>
2018-09-28 19:10:54 +0200

    Bonuses v8

--- basic/models/rest/Sms.php
+++ basic/models/rest/Sms.php
@@ -23,23 +23,23 @@ class Sms
 		if( $arSmsSettings['ACTIVE'] == 1 && !empty($arSmsSettings['API_ID']) ){
 			$arEvents = ClientsEvents::find()
 				->where(['EVENT_DATE' => date('Y-m-d', strtotime('+1 day'))])
-				->select(['PHONE' => 'clients.PHONE', 'CLIENT_NAME' => 'clients.NAME', 'EVENT' => 'events.NAME',  'RECIPIENT' => 'gift_recipients.NAME'])
+				->select(['PHONE' => 'clients.PHONE', 'BONUS' => 'clients.BONUS', 'CLIENT_NAME' => 'clients.NAME', 'EVENT' => 'events.NAME',  'RECIPIENT' => 'gift_recipients.NAME'])
 				->leftJoin('clients', 'clients.ID=clients_events.CLIENT_ID')
 				->leftJoin('events', 'events.ID=clients_events.EVENT_ID')
 				->leftJoin('gift_recipients', 'gift_recipients.ID=clients_events.GIFT_RECIPIENT_ID')
 				->asArray()
 				->all();
 
-			/*if( !empty($arEvents) ){
+			if( !empty($arEvents) ){
 				$obData = new \stdClass();
 				$obData->multiple = [];
 				foreach($arEvents as $arEvent){
 					$obSms = new SMSRU($arSmsSettings['API_ID']);
-					$obData->multi[$arEvent['PHONE']] = 'Добрый день, ' . $arEvent['CLIENT_NAME'] . '! :) Напоминаем о предстоящих важных событиях событиях. Дарим вам 200 бонусов на покупку букета до ' . date('d.m.Y', strtotime('+1 week'));
+					$obData->multi[$arEvent['PHONE']] = 'Добрый день, ' . $arEvent['CLIENT_NAME'] . '! :) Завтра у Вас знаменательное событие, начисляем 200 б. до ' . date('d.m.Y', strtotime('+1 week')) . '. Баланс - ' . $arEvents['BONUS'] . '. Ждем Вас за подарком.';
 				}
 
 				$obSms->send_one($obData);
-			}*/
+			}
 		}
 	}
 }
