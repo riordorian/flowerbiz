2a6e3722d3f0dff236e99d22a2093ab303708c96
riordorian <riordorian@mail.ru>
2018-09-28 20:06:30 +0200

    Bonuses v11

--- basic/views/admin/loyalty-programs/_form_steps.php
+++ basic/views/admin/loyalty-programs/_form_steps.php
@@ -8,7 +8,7 @@ use yii\widgets\ActiveForm;
 /* @var $form yii\widgets\ActiveForm */
 ?>
 
-<div class="loyalty-programs-steps-form row m-b"><?
+<!--<div class="loyalty-programs-steps-form row m-b"><?/*
 
     $form = ActiveForm::begin();
 
@@ -41,13 +41,13 @@ use yii\widgets\ActiveForm;
         ])->dropDownList($arGroups, ['prompt' => 'Выберите группу', 'class' => 'js-widget chosen']);
 
         if( !empty($modelStep->ID) ){
-            ?><div class="column_actions p-h-xxs">
-                <a href="/admin/loyalty-programs-steps/delete/" class="js-link_del" data-row-id="<?=$modelStep->ID?>">
+            */?><div class="column_actions p-h-xxs">
+                <a href="/admin/loyalty-programs-steps/delete/" class="js-link_del" data-row-id="<?/*=$modelStep->ID*/?>">
                     <i class="fa fa-times"></i>
                 </a>
-            </div><?
+            </div><?/*
         }
 
     ActiveForm::end();
 
-?></div>
+*/?></div>-->
